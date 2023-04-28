9c42ae7a27d93fe9f7c5f46f4ecb407042e9e90b
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-04-28 12:42:09 +0000

    other devices main structure (roq)

--- roqoqo_qiskit_devices/src/ibmq_belem.rs
+++ roqoqo_qiskit_devices/src/ibmq_belem.rs
@@ -9,3 +9,170 @@
 // License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 // express or implied. See the License for the specific language governing permissions and
 // limitations under the License.
+
+use std::collections::HashMap;
+
+use roqoqo::devices::{Device, GenericDevice};
+
+use ndarray::Array2;
+
+
+pub struct IBMBelemDevice { 
+    generic_device: GenericDevice
+}
+
+impl IBMBelemDevice {
+    /// Creates a new IBMLagos.
+    ///
+    /// # Returns
+    ///
+    /// An initiated IBMLagosDevice with single and two-qubit gates and decoherence rates set to zero.
+    ///
+    fn new() -> Self {
+        let generic = GenericDevice {
+            number_qubits: 5,
+            single_qubit_gates: HashMap::new(),
+            two_qubit_gates: HashMap::new(),
+            multi_qubit_gates: HashMap::new(),
+            decoherence_rates: HashMap::new(),
+        };
+        Self { generic_device: generic }
+    }
+}
+
+/// Implements Device trait for SquareLatticeDevice.
+///
+/// The Device trait defines standard functions available for roqoqo devices.
+///
+impl Device for IBMBelemDevice {
+    /// Returns the gate time of a single qubit operation if the single qubit operation is available on device.
+    ///
+    /// # Arguments
+    ///
+    /// * `hqslang` - The hqslang name of a single qubit gate.
+    /// * `qubit` - The qubit the gate acts on.
+    ///
+    /// # Returns
+    ///
+    /// * `Some<f64>` - The gate time.
+    /// * `None` - The gate is not available on the device.
+    ///
+    fn single_qubit_gate_time(&self, hqslang: &str, qubit: &usize) -> Option<f64> {
+        Some(0.0)
+    }
+
+    /// Returns the gate time of a two qubit operation if the two qubit operation is available on device.
+    ///
+    /// # Arguments
+    ///
+    /// * `hqslang` - The hqslang name of a two qubit gate.
+    /// * `control` - The control qubit the gate acts on.
+    /// * `target` - The target qubit the gate acts on.
+    ///
+    /// # Returns
+    ///
+    /// * `Some<f64>` - The gate time.
+    /// * `None` - The gate is not available on the device.
+    ///
+    fn two_qubit_gate_time(&self, hqslang: &str, control: &usize, target: &usize) -> Option<f64> {
+        Some(0.0)
+    }
+
+    /// Returns the gate time of a three qubit operation if the three qubit operation is available on device.
+    ///
+    /// # Arguments
+    ///
+    /// * `hqslang` - The hqslang name of a two qubit gate.
+    /// * `control_0` - The control_0 qubit the gate acts on.
+    /// * `control_1` - The control_1 qubit the gate acts on.
+    /// * `target` - The target qubit the gate acts on.
+    ///
+    /// # Returns
+    ///
+    /// * `Some<f64>` - The gate time.
+    /// * `None` - The gate is not available on the device.
+    ///
+    fn three_qubit_gate_time(
+        &self,
+        hqslang: &str,
+        control_0: &usize,
+        control_1: &usize,
+        target: &usize,
+    ) -> Option<f64> {
+        Some(0.0)
+    }
+    /// Returns the gate time of a multi qubit operation if the multi qubit operation is available on device.
+    ///
+    /// # Arguments
+    ///
+    /// * `hqslang` - The hqslang name of a multi qubit gate.
+    /// * `qubits` - The qubits the gate acts on.
+    ///
+    /// # Returns
+    ///
+    /// * `Some<f64>` - The gate time.
+    /// * `None` - The gate is not available on the device.
+    ///
+    fn multi_qubit_gate_time(&self, hqslang: &str, qubits: &[usize]) -> Option<f64> {
+        Some(0.0)
+    }
+
+    /// Returns the matrix of the decoherence rates of the Lindblad equation.
+    ///
+    /// # Arguments
+    ///
+    /// * `qubit` - The qubit for which the rate matrix is returned.
+    ///
+    /// # Returns
+    ///
+    /// * `Some<Array2<f64>>` - The decoherence rates.
+    /// * `None` - The qubit is not part of the device.
+    ///
+    fn qubit_decoherence_rates(&self, qubit: &usize) -> Option<Array2<f64>> {
+        Some(Array2::zeros((3, 3)))
+    }
+
+    /// Returns the number of qubits the device supports.
+    ///
+    /// # Returns
+    ///
+    /// `usize` - The number of qubits in the device.
+    ///
+    fn number_qubits(&self) -> usize {
+        self.generic_device.number_qubits
+    }
+
+    /// Returns the list of pairs of qubits linked with a native two-qubit-gate in the device.
+    ///
+    /// A pair of qubits is considered linked by a native two-qubit-gate if the device
+    /// can implement a two-qubit-gate between the two qubits without decomposing it
+    /// into a sequence of gates that involves a third qubit of the device.
+    /// The two-qubit-gate also has to form a universal set together with the available
+    /// single qubit gates.
+    ///
+    /// The returned vectors is a simple, graph-library independent, representation of
+    /// the undirected connectivity graph of the device.
+    /// It can be used to construct the connectivity graph in a graph library of the users
+    /// choice from a list of edges and can be used for applications like routing in quantum algorithms.
+    ///
+    /// # Returns
+    ///
+    /// A list (Vec) of pairs of qubits linked with a native two-qubit-gate in the device.
+    ///
+    fn two_qubit_edges(&self) -> Vec<(usize, usize)> {
+        vec![(0, 1), (1, 2), (1, 3), (3, 4)]
+    }
+
+    /// Turns Device into GenericDevice
+    ///
+    /// Can be used as a generic interface for devices when a boxed dyn trait object cannot be used
+    /// (for example when the interface needs to be serialized)
+    ///
+    /// # Note
+    ///
+    /// [crate::devices::GenericDevice] uses nested HashMaps to represent the most general device connectivity.
+    /// The memory usage will be inefficient for devices with large qubit numbers.
+    fn to_generic_device(&self) -> roqoqo::devices::GenericDevice {
+        self.generic_device.clone()
+    }
+}
