256bb5f5257df11fe671dcbbebb498b8af437201
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-05-08 09:56:45 +0000

    dev prop (temp)

--- qoqo_qiskit_devices/qoqo_qiskit_devices/device_property.py
+++ qoqo_qiskit_devices/qoqo_qiskit_devices/device_property.py
@@ -13,17 +13,45 @@
 
 from qoqo_qiskit_devices import ibm_devices
 
-from qiskit_ibm_provider import *
+from qiskit_ibm_provider import IBMProvider
+
+from typing import Union
+import types
 
 
 class DeviceProperties():
     """Utility for updating the properties on an IBMDevice instance."""
 
-    def __init__(self, device: ibm_devices) -> None:
+    def __init__(self, device: Union[types.ModuleType, str]) -> None:
         """Initialized DeviceProperty utility.
 
         Args:
-            device (ibm_devices): The qoqo_qiskit_devices instance that is to be updated.
-        """
-        self.device = device
+            device (Union[ibm_devices, str]): The qoqo_qiskit_devices instance (or name) that
+                is used to get the properties from IBM.
 
+        Raises:
+            TypeError: The input is not a string or an `ibm_devices` instance.
+        """
+        if isinstance(device, str):
+            self.properties = IBMProvider().get_backend(device).properties()
+        else:
+            try:
+                self.properties = IBMProvider().get_backend(device.name()).properties()
+            except Exception:
+                raise TypeError(
+                    'The input device is not a string or is not an `ibm_devices` instance.')
+
+    def update(self) -> ibm_devices:
+        """Updates the device information.
+
+        Returns:
+            ibm_devices: The updated device reference.
+        """
+        return self.device
+
+
+if __name__ == '__main__':
+    old_dev = ibm_devices.IBMBelemDevice()
+    devprop = DeviceProperties(old_dev)
+    new_dev = devprop.update()
+    print('ciao')
