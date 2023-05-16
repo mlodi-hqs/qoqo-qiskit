b86956ad37389b9200e6efca169fd211975f7908
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-05-16 15:33:32 +0000

    device_property script update

--- qoqo_qiskit_devices/qoqo_qiskit_devices/device_property.py
+++ qoqo_qiskit_devices/qoqo_qiskit_devices/device_property.py
@@ -11,15 +11,13 @@
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
 
-from qoqo_qiskit_devices import ibm_devices
-
 from qiskit_ibm_provider import IBMProvider
 
 from typing import Union
 import types
 
 
-class DeviceProperties():
+class DeviceProperties:
     """Utility for updating the properties on an IBMDevice instance."""
 
     def __init__(self, device: Union[types.ModuleType, str]) -> None:
@@ -34,24 +32,70 @@ class DeviceProperties():
         """
         if isinstance(device, str):
             self.properties = IBMProvider().get_backend(device).properties()
+            self.name = device
         else:
             try:
                 self.properties = IBMProvider().get_backend(device.name()).properties()
+                self.name = device.name()
             except Exception:
                 raise TypeError(
-                    'The input device is not a string or is not an `ibm_devices` instance.')
+                    "The input device is not a string or is not an `ibm_devices` instance."
+                )
+        self.configurations = IBMProvider().get_backend(self.name).configuration()
 
-    def update(self) -> ibm_devices:
-        """Updates the device information.
+    def update(self) -> None:
+        """Updates the device information."""
+        self.properties = IBMProvider().get_backend(self.name).properties(refresh=True)
+
+    def single_qubit_gate_time(self, gate: str, qubit: int) -> float:
+        """Returns the single-qubit gate time for a given gate and qubit.
+
+        Args:
+            gate (str): The input gate.
+            qubit (int): The input qubit.
+
+        Raises:
+            ValueError: The input gate is not available on the device.
+            ValueError: The input qubit is not available on the device.
 
         Returns:
-            ibm_devices: The updated device reference.
+            float: The gate time.
         """
-        return self.device
+        if qubit >= self.configurations.n_qubits:
+            raise ValueError("The input qubit is not available on the device.")
+        if gate not in self.configurations.basis_gates:
+            raise ValueError("The input gate is not available on the device.")
+        for entry in self.properties.gates:
+            if len(entry.qubits) == 1 and entry.qubits == qubit and entry.gate == gate:
+                return entry.parameters[1].value
 
+    def two_qubit_gate_time(self, gate: str, control: int, target: int) -> float:
+        """Returns the two-qubit gate time for a given gate and qubit.
 
-if __name__ == '__main__':
-    old_dev = ibm_devices.IBMBelemDevice()
-    devprop = DeviceProperties(old_dev)
-    new_dev = devprop.update()
-    print('ciao')
+        Args:
+            gate (str): The input gate.
+            control (int): The input control qubit.
+            target (int): The input target qubit.
+
+        Raises:
+            ValueError: The input gate is not available on the device.
+            ValueError: The input qubit is not available on the device.
+
+        Returns:
+            float: The gate time.
+        """
+        if (
+            control >= self.configurations.n_qubits
+            or target >= self.configurations.n_qubits
+        ):
+            raise ValueError("The input qubit is not available on the device.")
+        if gate not in self.configurations.basis_gates:
+            raise ValueError("The input gate is not available on the device.")
+        for entry in self.properties.gates:
+            if (
+                len(entry.qubits) == 2
+                and control in entry.qubits
+                and target in entry.qubits
+                and entry.gate == gate
+            ):
+                return entry.parameters[1].value
