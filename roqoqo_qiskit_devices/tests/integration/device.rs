ff9f50b84143effb2f503229d388f76c0433e00a
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-05-24 15:40:03 +0000

    added/modifies roqoqo_qiskit_devices tests

--- roqoqo_qiskit_devices/tests/integration/device.rs
+++ roqoqo_qiskit_devices/tests/integration/device.rs
@@ -10,7 +10,8 @@
 // express or implied. See the License for the specific language governing permissions and
 // limitations under the License.
 
-use ndarray::Array2;
+use ndarray::array;
+
 use roqoqo::devices::QoqoDevice;
 use roqoqo_qiskit_devices::*;
 
@@ -25,7 +26,39 @@ use test_case::test_case;
 #[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
 #[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
 fn test_single_qubit_gate_time(device: IBMDevice) {
-    assert_eq!(device.single_qubit_gate_time("PauliX", &0).unwrap(), 0.0);
+    assert_eq!(device.single_qubit_gate_time("PauliX", &0), None);
+}
+
+#[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
+#[test_case(IBMDevice::from(IBMJakartaDevice::new()); "JakartaDevice")]
+#[test_case(IBMDevice::from(IBMLagosDevice::new()); "LagosDevice")]
+#[test_case(IBMDevice::from(IBMLimaDevice::new()); "LimaDevice")]
+#[test_case(IBMDevice::from(IBMManilaDevice::new()); "ManilaDevice")]
+#[test_case(IBMDevice::from(IBMNairobiDevice::new()); "NairobiDevice")]
+#[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
+#[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
+fn test_set_single_qubit_gate_time(mut device: IBMDevice) {
+    assert!(device.set_single_qubit_gate_time("PauliX", 0, 0.5).is_ok());
+    assert_eq!(device.single_qubit_gate_time("PauliX", &0).unwrap(), 0.5);
+}
+
+#[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
+#[test_case(IBMDevice::from(IBMJakartaDevice::new()); "JakartaDevice")]
+#[test_case(IBMDevice::from(IBMLagosDevice::new()); "LagosDevice")]
+#[test_case(IBMDevice::from(IBMLimaDevice::new()); "LimaDevice")]
+#[test_case(IBMDevice::from(IBMManilaDevice::new()); "ManilaDevice")]
+#[test_case(IBMDevice::from(IBMNairobiDevice::new()); "NairobiDevice")]
+#[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
+#[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
+fn test_single_qubit_gate_names(device: IBMDevice) {
+    assert_eq!(
+        device.single_qubit_gate_names(),
+        vec![
+            "PauliX".to_string(),
+            "RotateZ".to_string(),
+            "SqrtPauliX".to_string(),
+        ]
+    );
 }
 
 #[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
@@ -37,7 +70,32 @@ fn test_single_qubit_gate_time(device: IBMDevice) {
 #[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
 #[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
 fn test_two_qubit_gate_time(device: IBMDevice) {
-    assert_eq!(device.two_qubit_gate_time("CNOT", &0, &1).unwrap(), 0.0);
+    assert_eq!(device.two_qubit_gate_time("CNOT", &0, &1), None);
+}
+
+#[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
+#[test_case(IBMDevice::from(IBMJakartaDevice::new()); "JakartaDevice")]
+#[test_case(IBMDevice::from(IBMLagosDevice::new()); "LagosDevice")]
+#[test_case(IBMDevice::from(IBMLimaDevice::new()); "LimaDevice")]
+#[test_case(IBMDevice::from(IBMManilaDevice::new()); "ManilaDevice")]
+#[test_case(IBMDevice::from(IBMNairobiDevice::new()); "NairobiDevice")]
+#[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
+#[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
+fn test_set_two_qubit_gate_time(mut device: IBMDevice) {
+    assert!(device.set_two_qubit_gate_time("CNOT", 0, 1, 0.5).is_ok());
+    assert_eq!(device.two_qubit_gate_time("CNOT", &0, &1).unwrap(), 0.5);
+}
+
+#[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
+#[test_case(IBMDevice::from(IBMJakartaDevice::new()); "JakartaDevice")]
+#[test_case(IBMDevice::from(IBMLagosDevice::new()); "LagosDevice")]
+#[test_case(IBMDevice::from(IBMLimaDevice::new()); "LimaDevice")]
+#[test_case(IBMDevice::from(IBMManilaDevice::new()); "ManilaDevice")]
+#[test_case(IBMDevice::from(IBMNairobiDevice::new()); "NairobiDevice")]
+#[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
+#[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
+fn test_two_qubit_gate_names(device: IBMDevice) {
+    assert_eq!(device.two_qubit_gate_names(), vec!["CNOT".to_string()]);
 }
 
 #[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
@@ -78,11 +136,20 @@ fn test_multi_qubit_gate_time(device: IBMDevice) {
 #[test_case(IBMDevice::from(IBMNairobiDevice::new()); "NairobiDevice")]
 #[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
 #[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
+fn test_multi_qubit_gate_names(device: IBMDevice) {
+    assert!(device.multi_qubit_gate_names().is_empty());
+}
+
+#[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
+#[test_case(IBMDevice::from(IBMJakartaDevice::new()); "JakartaDevice")]
+#[test_case(IBMDevice::from(IBMLagosDevice::new()); "LagosDevice")]
+#[test_case(IBMDevice::from(IBMLimaDevice::new()); "LimaDevice")]
+#[test_case(IBMDevice::from(IBMManilaDevice::new()); "ManilaDevice")]
+#[test_case(IBMDevice::from(IBMNairobiDevice::new()); "NairobiDevice")]
+#[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
+#[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
 fn test_qubit_decoherence_rates(device: IBMDevice) {
-    assert_eq!(
-        device.qubit_decoherence_rates(&0).unwrap(),
-        Array2::zeros((3, 3))
-    );
+    assert_eq!(device.qubit_decoherence_rates(&0), None);
 }
 
 #[test_case(IBMDevice::from(IBMBelemDevice::new()), 5; "BelemDevice")]
@@ -95,4 +162,72 @@ fn test_qubit_decoherence_rates(device: IBMDevice) {
 #[test_case(IBMDevice::from(IBMQuitoDevice::new()), 5; "QuitoDevice")]
 fn test_number_qubits(device: IBMDevice, qubits: usize) {
     assert_eq!(device.number_qubits(), qubits);
+}
+
+#[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
+#[test_case(IBMDevice::from(IBMJakartaDevice::new()); "JakartaDevice")]
+#[test_case(IBMDevice::from(IBMLagosDevice::new()); "LagosDevice")]
+#[test_case(IBMDevice::from(IBMLimaDevice::new()); "LimaDevice")]
+#[test_case(IBMDevice::from(IBMManilaDevice::new()); "ManilaDevice")]
+#[test_case(IBMDevice::from(IBMNairobiDevice::new()); "NairobiDevice")]
+#[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
+#[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
+fn test_add_damping(mut device: IBMDevice) {
+    device.add_damping(0, 0.5).unwrap();
+    assert_eq!(
+        device.qubit_decoherence_rates(&0).unwrap(),
+        array![[0.5, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+    )
+}
+
+#[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
+#[test_case(IBMDevice::from(IBMJakartaDevice::new()); "JakartaDevice")]
+#[test_case(IBMDevice::from(IBMLagosDevice::new()); "LagosDevice")]
+#[test_case(IBMDevice::from(IBMLimaDevice::new()); "LimaDevice")]
+#[test_case(IBMDevice::from(IBMManilaDevice::new()); "ManilaDevice")]
+#[test_case(IBMDevice::from(IBMNairobiDevice::new()); "NairobiDevice")]
+#[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
+#[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
+fn test_add_dephasing(mut device: IBMDevice) {
+    device.add_dephasing(0, 0.5).unwrap();
+    assert_eq!(
+        device.qubit_decoherence_rates(&0).unwrap(),
+        array![[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.5]]
+    )
+}
+
+#[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
+#[test_case(IBMDevice::from(IBMJakartaDevice::new()); "JakartaDevice")]
+#[test_case(IBMDevice::from(IBMLagosDevice::new()); "LagosDevice")]
+#[test_case(IBMDevice::from(IBMLimaDevice::new()); "LimaDevice")]
+#[test_case(IBMDevice::from(IBMManilaDevice::new()); "ManilaDevice")]
+#[test_case(IBMDevice::from(IBMNairobiDevice::new()); "NairobiDevice")]
+#[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
+#[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
+fn test_longest_chain(device: IBMDevice) {
+    assert!(!device.longest_chains().is_empty());
+}
+
+#[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
+#[test_case(IBMDevice::from(IBMJakartaDevice::new()); "JakartaDevice")]
+#[test_case(IBMDevice::from(IBMLagosDevice::new()); "LagosDevice")]
+#[test_case(IBMDevice::from(IBMLimaDevice::new()); "LimaDevice")]
+#[test_case(IBMDevice::from(IBMManilaDevice::new()); "ManilaDevice")]
+#[test_case(IBMDevice::from(IBMNairobiDevice::new()); "NairobiDevice")]
+#[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
+#[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
+fn test_longest_closed_chain(device: IBMDevice) {
+    assert!(device.longest_closed_chains().is_empty());
+}
+
+#[test_case(IBMDevice::from(IBMBelemDevice::new()); "BelemDevice")]
+#[test_case(IBMDevice::from(IBMJakartaDevice::new()); "JakartaDevice")]
+#[test_case(IBMDevice::from(IBMLagosDevice::new()); "LagosDevice")]
+#[test_case(IBMDevice::from(IBMLimaDevice::new()); "LimaDevice")]
+#[test_case(IBMDevice::from(IBMManilaDevice::new()); "ManilaDevice")]
+#[test_case(IBMDevice::from(IBMNairobiDevice::new()); "NairobiDevice")]
+#[test_case(IBMDevice::from(IBMPerthDevice::new()); "PerthDevice")]
+#[test_case(IBMDevice::from(IBMQuitoDevice::new()); "QuitoDevice")]
+fn test_edges(device: IBMDevice) {
+    assert!(!device.two_qubit_edges().is_empty());
 }
