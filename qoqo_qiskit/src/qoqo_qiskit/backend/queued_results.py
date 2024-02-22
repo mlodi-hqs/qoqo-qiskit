e3e927607fb89c7088848cedc0acc8814fcfd514
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2024-02-22 20:52:00 +0100

    QueuedCircuitRun

--- qoqo_qiskit/src/qoqo_qiskit/backend/queued_results.py
+++ qoqo_qiskit/src/qoqo_qiskit/backend/queued_results.py
@@ -11,7 +11,9 @@
 # the License.
 """Queued Jobs."""
 
-from qiskit.providers import Job
+from qiskit.providers import Job, JobStatus
+
+from .post_processing import _transform_job_result
 
 from typing import Any, List, Tuple, Dict, Optional
 
@@ -19,20 +21,54 @@ from typing import Any, List, Tuple, Dict, Optional
 class QueuedCircuitRun:
     """Queued Result of the circuit."""
 
-    def __init__(self, job: Job) -> None:
+    def __init__(
+        self,
+        job: Job,
+        memory: bool,
+        sim_type: str,
+        register_info: Tuple[
+            Dict[str, int],
+            Dict[str, List[List[bool]]],
+            Dict[str, List[List[float]]],
+            Dict[str, List[List[complex]]],
+        ],
+    ) -> None:
         """Initialise the QueuedCircuitRun class.
 
         Args:
-            job: The job that is run.
+            job (Job): The job that is run.
+            memory (bool): True if the result is meant to be read via `job.get_memory()` instead
+                of `job.get_counts()`.
+            sim_type (str): The simulation type. This can be "automatic", "statevector"
+                or "density_matrix".
+            register_info (Tuple[Any]): The initially setup registers.
+                These will be updated with the job result.
         """
-        self._job = job
+        self._job: Job = job
+        self._memory: bool = memory
+        self._sim_type: str = sim_type
+        self._initially_setup_registers: Tuple[
+            Dict[str, int],
+            Dict[str, List[List[bool]]],
+            Dict[str, List[List[float]]],
+            Dict[str, List[List[complex]]],
+        ] = register_info
+        self._qoqo_result: Optional[
+            Tuple[
+                Dict[str, List[List[bool]]],
+                Dict[str, List[List[float]]],
+                Dict[str, List[List[complex]]],
+            ]
+        ] = None
 
     def poll_result(
         self,
-    ) -> Tuple[
-        Dict[str, List[List[bool]]],
-        Dict[str, List[List[float]]],
-        Dict[str, List[List[complex]]],
+    ) -> Optional[
+        Tuple[
+            Dict[str, List[List[bool]]],
+            Dict[str, List[List[float]]],
+            Dict[str, List[List[complex]]],
+        ]
     ]:
         """Poll the result.
 
@@ -44,7 +80,28 @@ class QueuedCircuitRun:
         Raises:
             RuntimeError: The job failed or was cancelled.
         """
-        pass
+        if self._qoqo_result is not None:
+            return self._qoqo_result
+        if self._job.in_final_state():
+            status = self._job.status()
+            if status == JobStatus.DONE:
+                result = self._job.result()
+                self._qoqo_result = _transform_job_result(
+                    self._memory,
+                    self._sim_type,
+                    result,
+                    self._initially_setup_registers[0],
+                    self._initially_setup_registers[1],
+                    self._initially_setup_registers[2],
+                    self._initially_setup_registers[3],
+                )
+                return self._qoqo_result
+            elif status == JobStatus.ERROR:
+                RuntimeError("The job failed.")
+            else:
+                RuntimeError("The job was cancelled.")
+        else:
+            return None
 
 
 class QueuedProgramRun:
