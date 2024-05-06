4e338b2f4379eb76623a080990dc037d201c8df4
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2024-05-06 15:30:10 +0200

    clippy

--- roqoqo_qiskit_devices/src/devices/ibm_lagos.rs
+++ roqoqo_qiskit_devices/src/devices/ibm_lagos.rs
@@ -292,7 +292,7 @@ impl QoqoDevice for IBMLagosDevice {
     ///
     #[allow(unused_variables)]
     fn two_qubit_gate_time(&self, hqslang: &str, control: &usize, target: &usize) -> Option<f64> {
-        match self.two_qubit_gates.get(&hqslang.to_string()) {
+        match self.two_qubit_gates.get(hqslang) {
             Some(x) => x.get(&(*control, *target)).copied(),
             None => None,
         }
