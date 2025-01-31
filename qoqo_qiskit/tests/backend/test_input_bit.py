b42916be7a111ce03ca374c14b91fd934abf915f
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2025-01-31 16:54:00 +0100

    copyright

--- qoqo_qiskit/tests/backend/test_input_bit.py
+++ qoqo_qiskit/tests/backend/test_input_bit.py
@@ -1,4 +1,4 @@
-# Copyright © 2024 HQS Quantum Simulations GmbH.
+# Copyright © 2024-2025 HQS Quantum Simulations GmbH.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -10,6 +10,7 @@
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
 """Test running local operation with qasm backend."""
+
 from qoqo_qiskit import QoqoQiskitBackend
 from qoqo import Circuit, QuantumProgram
 from qoqo.measurements import ClassicalRegister, PauliZProductInput, PauliZProduct  # type:ignore
