2d912c209e94111b0cbad9aaa508dfe3672562e9
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2025-11-04 08:35:55 +0100

    double PauliProduct testing

--- qoqo_qiskit/tests/utils/test_utils.py
+++ qoqo_qiskit/tests/utils/test_utils.py
@@ -19,7 +19,7 @@ from struqture_py.spins import PauliHamiltonian, PauliProduct  # type:ignore
 
 
 def test_basic_hamiltonian() -> None:
-    """Test the basic Hamiltonian conversion."""
+    """Test struqture_hamiltonian_to_qiskit_op with a basic Hamiltonian."""
     pp = PauliProduct().x(0).z(1).y(2)
 
     hamiltonian = PauliHamiltonian()
@@ -32,16 +32,18 @@ def test_basic_hamiltonian() -> None:
 
 
 def test_big_hamiltonian() -> None:
-    """Test the big Hamiltonian conversion."""
+    """Test struqture_hamiltonian_to_qiskit_op with a big Hamiltonian."""
     pp = PauliProduct().x(0).z(1).y(2).x(3).z(4).y(5).x(6).z(7).y(8).x(9).z(10).y(11)
+    pp2 = PauliProduct().x(12)
 
     hamiltonian = PauliHamiltonian()
     hamiltonian.add_operator_product(pp, 0.5)
+    hamiltonian.add_operator_product(pp2, 0.25)
 
-    res = struqture_hamiltonian_to_qiskit_op(hamiltonian, 12)
+    res = struqture_hamiltonian_to_qiskit_op(hamiltonian, 13)
 
-    assert res.num_qubits == 12
-    assert res.to_list() == [("YZXYZXYZXYZX", (0.5 + 0j))]
+    assert res.num_qubits == 13
+    assert res.to_list() == [("IYZXYZXYZXYZX", (0.5 + 0j)), ("XIIIIIIIIIIII", (0.25 + 0j))]
 
 
 # For pytest
