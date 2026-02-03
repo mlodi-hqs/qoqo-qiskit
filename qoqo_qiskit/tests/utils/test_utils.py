d7f06653718b79fa25cf373c511c7dcdce9bab7f
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2026-02-03 14:14:40 +0100

    first tests measure_spin_op

--- qoqo_qiskit/tests/utils/test_utils.py
+++ qoqo_qiskit/tests/utils/test_utils.py
@@ -14,7 +14,11 @@
 import pytest
 import sys
 
-from qoqo_qiskit.utils import struqture_hamiltonian_to_qiskit_op
+from qoqo_qiskit.utils import (
+    struqture_hamiltonian_to_qiskit_op,
+    measure_spin_operator_to_qiskit,
+    _sort_by_length,
+)
 from struqture_py.spins import PauliHamiltonian, PauliProduct  # type:ignore
 
 
@@ -45,6 +49,49 @@ def test_big_hamiltonian() -> None:
     assert res.num_qubits == 13
     assert res.to_list() == [("IYZXYZXYZXYZX", (0.5 + 0j)), ("XIIIIIIIIIIII", (0.25 + 0j))]
 
+
+def test_measure_spin_operator_empty() -> None:
+    """Test measure_spin_operator with an empty operator."""
+    pp = PauliProduct().x(0).z(1)
+    hamiltonian = PauliHamiltonian()
+    hamiltonian.add_operator_product(pp, 0.5)
+
+    with pytest.raises(ValueError) as exc:
+        _ = measure_spin_operator_to_qiskit(hamiltonian, "empty", False, definitionbit_length=1)
+    assert (
+        "The number of spins in the operators passed is \
+            2. The length of the \
+            DefinitionBit input is 1, which is smaller. \
+            The measurement can therefore not be constructed."
+        in str(exc.value)
+    )
+
+
+def test_sort_by_length() -> None:
+    """Test _sort_by_length function."""
+    pp_1 = PauliProduct().x(0).z(1).y(4)
+    pp_2 = PauliProduct().x(0).z(2).y(4)
+    pp_3 = PauliProduct().x(0).y(1)
+
+    hamiltonian_1 = PauliHamiltonian()
+    hamiltonian_1.add_operator_product(pp_1, 0.5)
+
+    hamiltonian_2 = PauliHamiltonian()
+    hamiltonian_2.add_operator_product(pp_1, 0.5)
+    hamiltonian_2.add_operator_product(pp_2, 0.5)
+
+    hamiltonian_3 = PauliHamiltonian()
+    hamiltonian_3.add_operator_product(pp_1, 0.5)
+    hamiltonian_3.add_operator_product(pp_3, 0.5)
+
+    hamiltonian_4 = PauliHamiltonian()
+    hamiltonian_4.add_operator_product(pp_3, 0.5)
+    hamiltonian_4.add_operator_product(pp_1, 0.5)
+
+    assert _sort_by_length(hamiltonian_1) == [pp_1]
+    assert _sort_by_length(hamiltonian_2) == [pp_2, pp_1]
+    assert _sort_by_length(hamiltonian_3) == [pp_1, pp_3]
+    assert _sort_by_length(hamiltonian_4) == [pp_1, pp_3]
 
 # For pytest
 if __name__ == "__main__":
